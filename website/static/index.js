f966892cbd80bb9c92f8ecef53effc76d33fce85
Caroline <zhucaroline18@gmail.com>
2022-01-16 21:45:46 -0800

    added strikethrough

--- website/static/index.js
+++ website/static/index.js
@@ -6,5 +6,16 @@ function deleteNote(noteId){
         window.location.href = "/";
     });
 }
+
+function strikethrough(node) {
+    if (node.checked) {
+        console.log("Checked")
+        node.parentNode.style.textDecoration  = "line-through";
+    }
+    else {
+        node.parentNode.style.textDecoration  = "";
+        console.log("Unchecked");
+    }
+}
 //takes note id we pass and send post request to delete note endpoint
 //after response, reload window and redirects to home page and refreshes page 
